d082bbb95312f10e84b2e13bd9e2af67459d1ad5
Alec <amsears60441@gmail.com>
2024-01-26 17:14:05 -0700

    got rid of hello world page used for testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { ReactComponent as MyIcon } from './reddit.svg';
 import RedditPopular from './components/RedditPopular';
-import HelloWorld from './components/HelloWorld'; // Import HelloWorld component
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import CommentsPage from './components/CommentsPage'; // Import the CommentsPage component
 
@@ -14,13 +13,11 @@ function App() {
            <h1 className='display-6'>Simple <MyIcon className="myIconSize" /> Reddit</h1>
           <nav>
             <Link className='btn btn-dark me-2' to="/">Popular Posts</Link>
-            <Link className='btn btn-dark me-2' to="/hello-world">Hello World</Link>
           </nav>
         </header>
 
         <Routes>
           <Route path="/" element={<RedditPopular />} />
-          <Route path="/hello-world" element={<HelloWorld />} />
           <Route path="/comments/:postId" element={<CommentsPage />} />
         </Routes>
       </div>
