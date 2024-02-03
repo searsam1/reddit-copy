9ec9c08b586bd126166853ece215824c2d0507d9
Alec <amsears60441@gmail.com>
2024-02-03 12:57:54 -0700

    put on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import { ReactComponent as MyIcon } from '../reddit.svg';
+import homeIcon from '../redditLOGO.webp'; // Import the .webp image
+
 export function Home() {
     return (
         <>
@@ -8,7 +11,10 @@ export function Home() {
             <p style={{fontSize:"25px"}} className='m-5 text-white text-center'>
                 Click on a <span className='text-danger'>subreddit</span> in the navbar to get started!
             </p>
-            
+            {/* Use an img tag for .webp images */}
+            <div className="home-img">
+              <img src={homeIcon} alt="Home Icon" />
+            </div>
         </>
     )
 }
